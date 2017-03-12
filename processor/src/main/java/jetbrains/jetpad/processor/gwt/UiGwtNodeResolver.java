8c83702a68e4673e7562c7938626730a4a4120ad
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2017-03-12 18:49:22 +0100

    now we can gererate arbitrary T extends Element type

--- processor/src/main/java/jetbrains/jetpad/processor/gwt/UiGwtNodeResolver.java
+++ processor/src/main/java/jetbrains/jetpad/processor/gwt/UiGwtNodeResolver.java
@@ -1,14 +1,17 @@
 package jetbrains.jetpad.processor.gwt;
 
-import com.google.gwt.dom.client.Element;
 import jetbrains.jetpad.processor.gwt.metadata.FieldData;
 import jetbrains.jetpad.processor.gwt.metadata.GwtFieldData;
+import org.w3c.dom.Attr;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
+import com.google.gwt.dom.client.Element;
 
 public class UiGwtNodeResolver extends NodeResolver<List<FieldData<Element>>> {
 
@@ -36,7 +39,16 @@ public class UiGwtNodeResolver extends NodeResolver<List<FieldData<Element>>> {
     if (isFieldDataNode(node.getFirstChild())) {
       Node fieldData = node.getFirstChild();
       NamedNodeMap attributes = fieldData.getAttributes();
-      return new GwtFieldData<Element>(attributes.getNamedItem("name").getNodeValue(), Element.class);
+      Class<Element> clazz = Element.class;
+      Node type = attributes.getNamedItem("type");
+      if (type != null) {
+        try {
+          clazz = (Class<Element>) Class.forName(String.format("com.google.gwt.dom.client.%s", type.getNodeValue()));
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        }
+      }
+      return new GwtFieldData<Element>(attributes.getNamedItem("name").getNodeValue(), clazz);
     }
 
     return null;
