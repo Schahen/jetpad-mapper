1357fa6b8cda91667a32a72484b412e861753300
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2017-03-12 20:29:10 +0100

    parametrize Mapper

--- processor/src/main/java/jetbrains/jetpad/processor/gwt/MapperGenerator.java
+++ processor/src/main/java/jetbrains/jetpad/processor/gwt/MapperGenerator.java
@@ -1,9 +1,13 @@
 package jetbrains.jetpad.processor.gwt;
 
 import com.google.gwt.dom.client.Element;
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeVariableName;
 import jetbrains.jetpad.mapper.Mapper;
 import jetbrains.jetpad.processor.gwt.metadata.FieldData;
 
@@ -19,8 +23,19 @@ public class MapperGenerator {
   }
 
   public void generate(String packageName, String className, Appendable out) throws IOException {
-    System.out.println("==============================" + className);
-    TypeSpec.Builder mapperSpecBuilder = TypeSpec.classBuilder(className).addModifiers(Modifier.PUBLIC);
+
+
+    System.out.println("---------------------------------------------");
+
+
+    TypeVariableName targetType = TypeVariableName.get("T");
+    TypeVariableName sourceType = TypeVariableName.get("S");
+
+    TypeSpec.Builder mapperSpecBuilder = TypeSpec.classBuilder(className)
+        .addTypeVariable(targetType)
+        .addTypeVariable(sourceType)
+        .superclass(ParameterizedTypeName.get(ClassName.get(Mapper.class), targetType, sourceType))
+        .addModifiers(Modifier.PUBLIC);
 
     mapperSpecBuilder.addMethod(MethodSpec
         .methodBuilder("registerSynchronizers")
