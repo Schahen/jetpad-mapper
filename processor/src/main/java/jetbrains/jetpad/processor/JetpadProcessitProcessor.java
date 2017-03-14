730c7ff43cf9ef200dd3aee70f077ca38256ba06
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2017-03-14 16:25:29 +0100

    as of no this module is completeley separated from the rest of the code

--- processor/src/main/java/jetbrains/jetpad/processor/JetpadProcessitProcessor.java
+++ processor/src/main/java/jetbrains/jetpad/processor/JetpadProcessitProcessor.java
@@ -1,13 +1,20 @@
 package jetbrains.jetpad.processor;
 
+import jetbrains.jetpad.processor.gwt.UiGwtGenerator;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.shabunc.maven.Processor;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class JetpadProcessitProcessor implements Processor {
@@ -24,7 +31,34 @@ public class JetpadProcessitProcessor implements Processor {
 
   @Override
   public void process(File file) {
-    System.out.println("========= PROCESSING ======= " + file.getName());
+
+    String fullPath = file.toString();
+    String trimmedPath = fullPath.replace(".jetpad.xml", "").replaceFirst("src/main/java/", "");
+
+    int lastSeparatorIndex = trimmedPath.lastIndexOf('/');
+    String packagePath = trimmedPath.substring(0, lastSeparatorIndex);
+    String shortClassName = trimmedPath.substring(lastSeparatorIndex + 1);
+
+    String packageName = packagePath.replace("/", ".");
+    UiGwtGenerator uiGwtGenerator = new UiGwtGenerator(packageName, shortClassName);
+
+    ByteArrayOutputStream viewOutputStream = new ByteArrayOutputStream();
+    ByteArrayOutputStream uiXmlOutputStream = new ByteArrayOutputStream();
+    ByteArrayOutputStream modelStream = new ByteArrayOutputStream();
+    ByteArrayOutputStream mapperStream = new ByteArrayOutputStream();
+
+    try {
+      uiGwtGenerator.generate(file, uiXmlOutputStream, viewOutputStream, modelStream, mapperStream);
+      System.out.println(uiGwtGenerator);
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    } catch (SAXException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (TransformerException e) {
+      e.printStackTrace();
+    }
   }
 
 }
