fb13073fcb662a9a0ae777cf55ed3753f30625e3
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2017-03-23 12:50:46 +0300

    [issue-1] passing model adn event as params

--- processor/src/main/java/jetbrains/jetpad/processor/gwt/metadata/events/AbstractEventData.java
+++ processor/src/main/java/jetbrains/jetpad/processor/gwt/metadata/events/AbstractEventData.java
@@ -1,6 +1,7 @@
 package jetbrains.jetpad.processor.gwt.metadata.events;
 
 
+import com.google.gwt.user.client.Event;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
@@ -20,6 +21,8 @@ public abstract class AbstractEventData implements EventData {
   @Override
   public TypeSpec.Builder addHandler(TypeSpec.Builder typeSpec, ClassName modelClassName) {
     MethodSpec.Builder addHandlerBuilder = MethodSpec.methodBuilder(getHandlerName());
+    addHandlerBuilder.addParameter(modelClassName, "model");
+    addHandlerBuilder.addParameter(Event.class, "evt");
     addHandlerBuilder.addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT).returns(boolean.class);
     typeSpec.addMethod(addHandlerBuilder.build());
     return typeSpec;
